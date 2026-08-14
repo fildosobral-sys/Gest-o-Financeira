f925c4634e9292c4f4209485766542510202d7a8
fildosobral-sys <fildosobral@gmail.com>
2026-08-14 09:46:12 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'meu-financeiro-v4-20260814';
-const ASSETS = ['./', './index.html', './styles.css', './managers.css', './manifest.json', './js/app.js', './js/domain.js', './js/storage.js', './icon-180.png', './icon-192.png', './icon-512.png'];
+const CACHE = 'meu-financeiro-v4.2-20260814';
+const ASSETS = ['./', './index.html', './styles.css', './managers.css', './ui-fixes.css', './manifest.json', './js/app.js', './js/domain.js', './js/storage.js', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
